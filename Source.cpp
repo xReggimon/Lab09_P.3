74d81e72538f9409b13be3be7a5629ef90a65806
xReggimon <r-mcmullen.1@onu.edu>
2024-04-11 03:27:48 -0400

    Completed displayRun

---
+++ Source.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include<random>
+
+using namespace std;
+
+void displayRun(int values[], int size);
+//bool hasRun(int values[], int size);
+int rollDice();
+
+int main() {
+
+	int const arraySize = 20;
+	int randArray[arraySize];
+	srand(time(0));
+
+	do {
+		for (int i = 0; i < arraySize; i++) {
+			randArray[i] = rollDice();
+		}
+		displayRun(randArray, arraySize);
+		cout << endl;
+	} while (true);
+	
+}
+
+void displayRun(int values[], int size) {
+	int parenthHold = values[0];
+	bool continueRun = false;
+	
+	for (int i = 1; i < size + 1; i++) {
+		if (continueRun) {
+			if (values[i] == parenthHold) {
+				cout << parenthHold;
+			}
+			else {
+				cout << parenthHold << ")";
+			}
+		}
+		else {
+			if (values[i] == parenthHold) {
+				cout << "(" << parenthHold;
+				continueRun = true;
+			} else {
+				cout << parenthHold;
+			} 
+		}
+		parenthHold = values[i];
+		cout << " ";
+	}
+}
